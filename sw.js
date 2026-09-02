f60ee811fa24d8c2024809dc716dcd61d674bd74
yapiassamoi90-boop <yapiassamoi90@gmail.com>
2026-09-02 22:51:05 +0000

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'carena-maintenance-v1';
+const assetsToCache = [
+  'index.html',
+  'app.js',
+  'manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
